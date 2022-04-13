8d31245c1bd5eb636529f587ad03b598223d72b1
username <956465331@qq.com>
2022-04-13 22:01:49 +0800

    Site updated: 2022-04-13 22:01:32

--- js/randombg.js
+++ js/randombg.js
@@ -1,12 +1,12 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
+    
     "url(/img/bg1.png)",
     "url(/img/bg2.png)",
     "url(/img/bg3.png)",
     "url(/img/bg4.png)",
-    "url(/img/bg5.jpg)",
     "url(/img/bg6.png)",
-    "url(/img/bg7.png)"
+    "url(/img/bg7.webp)"
   ];
   //获取背景图片总数，生成随机数
   var bgindex =Math.floor(Math.random() * (backimg.length));
@@ -14,13 +14,13 @@ var backimg =[
   document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
   //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
   var bannerimg =[
+  
     "url(/img/bg1.png)",
     "url(/img/bg2.png)",
     "url(/img/bg3.png)",
     "url(/img/bg4.png)",
-    "url(/img/bg5.jpg)",
     "url(/img/bg6.png)",
-    "url(/img/bg7.png)"
+    "url(/img/bg7.webp)"
   ];
   //获取banner图片总数，生成随机数
   var bannerindex =Math.ceil(Math.random() * (bannerimg.length-1));
