443f2094bd6d9414c9b6f42b764349f8fbe8cb82
gallifreyCar <956465331@qq.com>
2023-04-13 10:08:23 +0800

    Site updated: 2023-04-13 10:08:21

--- js/randombg.js
+++ js/randombg.js
@@ -1,31 +1,30 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
-var backimg =[
-    
-    "url(/img/bg1.png)",
-    "url(/img/bg2.png)",
-    "url(/img/bg3.png)",
-    "url(/img/bg4.png)",
-    "url(/img/bg6.png)",
-    "url(/img/bg7.webp)"
-  ];
-  //获取背景图片总数，生成随机数
-  var bgindex =Math.floor(Math.random() * (backimg.length));
-  //重设背景图片
-  document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
-  //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
-  var bannerimg =[
-  
-    "url(/img/bg1.png)",
-    "url(/img/bg2.png)",
-    "url(/img/bg3.png)",
-    "url(/img/bg4.png)",
-    "url(/img/bg6.png)",
-    "url(/img/bg7.webp)"
-  ];
-  //获取banner图片总数，生成随机数
-  var bannerindex =Math.ceil(Math.random() * (bannerimg.length-1));
-  //重设banner图片
-  document.getElementById("page-header").style.backgroundImage = bannerimg[bannerindex];
+var backimg = [
+
+  "url(/img/bg1.webp)",
+  "url(/img/bg2.webp)",
+  "url(/img/bg3.webp)",
+  "url(/img/bg4.webp)",
+  "url(/img/bg6.webp)",
+  "url(/img/bg7.webp)"
+];
+//获取背景图片总数，生成随机数
+var bgindex = Math.floor(Math.random() * (backimg.length));
+//重设背景图片
+document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
+//随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
+var bannerimg = [
+
+  "url(/img/bg1.webp)",
+  "url(/img/bg2.webp)",
+  "url(/img/bg3.webp)",
+  "url(/img/bg4.webp)",
+  "url(/img/bg6.webp)",
+  "url(/img/bg7.webp)"
+];
+//获取banner图片总数，生成随机数
+var bannerindex = Math.ceil(Math.random() * (bannerimg.length - 1));
+//重设banner图片
+document.getElementById("page-header").style.backgroundImage = bannerimg[bannerindex];
 
 
-  
