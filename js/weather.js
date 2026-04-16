e33b519488c4e4bff5aabf2e9e61f80307b4af13
gallifreyCar <956465331@qq.com>
2026-04-16 04:53:37 +0000

    Site updated: 2026-04-16 04:53:31

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "000000",
+    "tmpSize": "16",
+    "cityColor": "000000",
+    "citySize": "16",
+    "aqiColor": "000000",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "b5acaeddded045c6a272d9c924438d8e"
+  }
+}
